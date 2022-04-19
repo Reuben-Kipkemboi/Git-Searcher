d592383dc4ec0c3a74c5a9d8e3b9ab20f79888a1
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-19 20:00:31 +0300

    st:nav-bar - configures routing options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { SearchComponent } from './Components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // path to our home page
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  // path to our main page
+  {
+    path: "main",
+    component: SearchComponent,
+  },
+  // route  to our github users details page
+  { path: '', redirectTo: "/home", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
