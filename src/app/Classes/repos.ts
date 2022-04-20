0580255b69a5b43c9babe39d8f5490eec3c569de
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-20 06:41:26 +0300

    ft:class - updates  repository  class

--- src/app/Classes/repos.ts
+++ src/app/Classes/repos.ts
@@ -1,2 +1,11 @@
 export class Repos {
+    constructor (
+        public  name:string,
+        public  description:string,
+        public  fork:string,
+        public  stargazers_count:string,
+        public  clone_url:string,
+        public  language:string,
+
+    ) {}
 }
